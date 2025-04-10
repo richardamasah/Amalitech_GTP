e194d4aefe0a8676d9e62eb88a8a7df1cb1563d6
djanie <richard.amasah@amalitechtraining.org>
2025-04-10 15:49:32 +0000

    jupyternotebook with project overview

--- src/test.py
+++ src/test.py
@@ -23,6 +23,17 @@ plt.ylabel('Average Rating')
 plt.tight_layout()
 plt.show()
 
+#revenue vs budget
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=df, x='budget_musd', y='revenue_musd', hue='original_language', alpha=0.7)
+plt.title('Revenue vs Budget')
+plt.xlabel('Budget (Million USD)')
+plt.ylabel('Revenue (Million USD)')
+plt.legend(title='Language', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+plt.show()
+
+
 
 
 
